6575bb6e5bff01861a5df8f4fefa397c1b9a8cd9
Jeremy Lu <DrkCoater@gmail.com>
2017-07-21 20:22:42 -0700

    spacing

--- Student/src/us/csbu/cs546/algorithm/FibonacciCoefficients.java
+++ Student/src/us/csbu/cs546/algorithm/FibonacciCoefficients.java
@@ -2,31 +2,65 @@ package us.csbu.cs546.algorithm;
 
 import java.util.Scanner;
 
+// import java.util.Scanner;
+
 public class FibonacciCoefficients {
-	public static void main(String string[]) {
-		fibonacci();
-	}
 	
-	public static void fibonacci()
-    {
-        int i, space, rows, k=0;
+	private int[] cache;
+	
+	public static void main(String string[]) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter Number of Rows : ");
-        rows = scan.nextInt();
-        for(i=1; i<=rows; i++)
-        {
-            for(space=1; space<=(rows-i); space++)
-            {
+        int rows = scan.nextInt();
+        scan.close();
+		comp1(rows);
+		System.out.println();
+		comp2(rows);
+	}
+	
+	public static int fibonacci(int input) {
+		if (input <= 1) {
+			return input;
+		}
+		return fibonacci(input-1) + fibonacci(input - 2);
+    }
+
+	public static void comp1(int rows) {
+		int i, space, k=1;
+        for(i=1; i<=rows; i++) {
+            for(space=1; space<=(rows-i); space++) {
                 System.out.print("  ");
             }
-            while(k != (2*i-1))
-            {
-                System.out.print(k + " ");
+            while(k != ( 2*i )) {
+                System.out.print(fibonacci(k) + " ");
                 k++;
             }
-            k = 0;
+            k = 1;
             System.out.println();
         }
-        scan.close();
-    }
+	}
+	
+	public int fib2(int input) {
+		if (input <= 1) {
+			this.cache[input] = input;
+			return input;
+		}
+		this.cache[input] = this.cache[input-1] + this.cache[input-2];
+		return this.cache[input];
+	}
+	
+	public static void comp2(int rows) {
+		int i, space, k=1;
+        for(i=1; i<=rows; i++) {
+            for(space=1; space<=(rows-i); space++) {
+                System.out.print("  ");
+            }
+            while(k != ( 2*i )) {
+                System.out.print(fibonacci(k) + " ");
+                k++;
+            }
+            k = 1;
+            System.out.println();
+        }
+	}
 }
